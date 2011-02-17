3a02fc25e9e446af3afd98b28ddf2454c7d7f2b2
lidgren <lidgren@users.noreply.github.com>
2011-02-17 17:23:35 +0000

    Added NetOutgoingMessage.Write(NetIncomingMessage)

--- Lidgren.Network/NetOutgoingMessage.Write.cs
+++ Lidgren.Network/NetOutgoingMessage.Write.cs
@@ -598,5 +598,23 @@ namespace Lidgren.Network
 				m_bitLength -= excessBits;
 			}
 		}
+
+		/// <summary>
+		/// Append all the bits of message to this message
+		/// </summary>
+		public void Write(NetIncomingMessage message)
+		{
+			EnsureBufferSize(m_bitLength + (message.LengthBytes * 8));
+
+			Write(message.m_data, 0, message.LengthBytes);
+
+			// did we write excessive bits?
+			int bitsInLastByte = (message.m_bitLength % 8);
+			if (bitsInLastByte != 0)
+			{
+				int excessBits = 8 - bitsInLastByte;
+				m_bitLength -= excessBits;
+			}
+		}
 	}
 }
